8da2e554a5f66355adf11b6a6bf6c155145ca126
younoffice <yuhki20@gmail.com>
2026-08-20 17:30:20 +0900

    Add Microsoft lockup to favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon-v2.svg", type: "image/svg+xml" },
-      { url: "/favicon-v2-32.png", sizes: "32x32", type: "image/png" },
-      { url: "/icon-v2-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/favicon-v2-512.png", sizes: "512x512", type: "image/png" },
+      { url: "/favicon-v3.svg", type: "image/svg+xml" },
+      { url: "/favicon-v3-32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon-v3-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/favicon-v3-512.png", sizes: "512x512", type: "image/png" },
     ],
-    apple: [{ url: "/apple-touch-icon-v2.png", sizes: "180x180", type: "image/png" }],
+    apple: [{ url: "/apple-touch-icon-v3.png", sizes: "180x180", type: "image/png" }],
   },
 };
 
